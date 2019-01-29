28347daa6905e5bd49c23ffcce156f176ca1a9f1
Nicolas <zhaoxd@zenmen.com>
2019-01-29 10:36:30 +0800

    chore: add example

--- example/index.js
+++ example/index.js
@@ -1,1 +1,8 @@
-const a = () => {};
+let e = {
+  oo: 1, 
+  bar: 2, 
+  c: 3, 
+  d: 4
+};
+
+`console.log(e);console.log(e);console.log(e);console.log(e);console.log(e);console.log(e);console.log;`;
