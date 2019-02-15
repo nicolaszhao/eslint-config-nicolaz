451a3a5bc9ff8c365512f3a6a6b8ae2aaa5d28e9
Nicolas <zhaoxd@zenmen.com>
2019-02-15 18:35:47 +0800

    feat: 分离 base 规则

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
 module.exports = {
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
     ecmaFeatures: {
       jsx: true
     }
   },
-  env: {
-    browser: true,
-    node: true,
-    commonjs: true,
-    es6: true,
-    worker: true,
-    amd: true,
-    mocha: true,
-    jest: true,
-    qunit: true,
-    jquery: true,
-    shelljs: true
-  },
   plugins: [
-    'import',
     'react',
     'jsx-a11y'
   ],
   extends: [
-    'eslint:recommended',
+    'eslint-config-nz-base',
     'plugin:react/recommended',
-    './rules/errors.js',
-    './rules/best-practices.js',
-    './rules/variables.js',
-    './rules/style.js',
-    './rules/es6.js',
-    './rules/import.js',
-    './rules/jsx-a11y.js',
-    './rules/react.js'
+    './rules/react.js',
+    './rules/jsx-a11y.js'
   ]
 };
