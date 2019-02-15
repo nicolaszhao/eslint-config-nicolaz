3b38220291473a81b06369bc690bf3819e9bb13c
Nicolas <zhaoxd@zenmen.com>
2019-02-15 18:33:45 +0800

    chore: 提取 base 配置到独立 npm 包

--- base/rules/variables.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  rules: {
-    
-    // 不允许在变量定义之前使用它们
-    'no-use-before-define': ['error', 'nofunc']
-  }
-};
--- rules/errors.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  rules: {
-
-    // 是否禁用 console
-    'no-console': 'warn'
-  }
-};
