4484d41d4c6941b04a16e3643e7acabce6353b87
Nicolas <zhaoxd@zenmen.com>
2019-10-28 14:55:37 +0800

    feat: 修改 jsx-filename-extension 规则

--- packages/eslint-config-nicolaz/index.js
+++ packages/eslint-config-nicolaz/index.js
@@ -5,6 +5,10 @@ module.exports = {
     'nicolaz-base'
   ],
   rules: {
+
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+
     'react-hooks/exhaustive-deps': 'warn'
   }
 };
