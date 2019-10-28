dd4c8d0e426666e34cf63977d78ca55aca5acbc4
Nicolas <zhaoxd@zenmen.com>
2019-10-28 19:40:21 +0800

    feat: 调整几个规则

--- packages/eslint-config-nicolaz-base/index.js
+++ packages/eslint-config-nicolaz-base/index.js
@@ -2,6 +2,9 @@ module.exports = {
   extends: [
     'airbnb-base'
   ],
+  env: {
+    browser: true
+  },
   rules: {
 
     // 允许对函数参数重新赋值
@@ -16,9 +19,28 @@ module.exports = {
     // https://cn.eslint.org/docs/rules/global-require
     'global-require': 'off',
 
+    // 允许使用非 Error 对象作为 Promise 拒绝的原因
+    // https://cn.eslint.org/docs/rules/prefer-promise-reject-errors
     'prefer-promise-reject-errors': 'off',
+
+    // 允许使用嵌套的三元表达式
+    // https://cn.eslint.org/docs/rules/no-nested-ternary
     'no-nested-ternary': 'off',
 
+    // 禁止未使用过的表达式，但允许在表达式中使用逻辑短路求值
+    // https://cn.eslint.org/docs/rules/no-unused-expressions
+    'no-unused-expressions': ['error', {
+      allowShortCircuit: true,
+      allowTernary: false,
+      allowTaggedTemplates: false,
+    }],
+
+    // 允许使用一元操作符 ++ 和 --
+    // https://cn.eslint.org/docs/rules/no-plusplus
+    'no-plusplus': 'off',
+
+    // 允许 require 的参数是动态生成的
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-dynamic-require.md
     'import/no-dynamic-require': 'off',
   }
 };
--- packages/eslint-config-nicolaz/index.js
+++ packages/eslint-config-nicolaz/index.js
@@ -9,6 +9,8 @@ module.exports = {
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
 
+    // https://www.npmjs.com/package/eslint-plugin-react-hooks
+    // https://reactjs.org/docs/hooks-rules.html
     'react-hooks/exhaustive-deps': 'warn'
   }
 };
