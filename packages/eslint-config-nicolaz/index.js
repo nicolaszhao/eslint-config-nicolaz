eca06cc22978cf103ec833751fe455d322a27ff3
Nicolas <zhaoxd@zenmen.com>
2019-11-18 15:41:38 +0800

    feat: off react/prop-types

--- packages/eslint-config-nicolaz/index.js
+++ packages/eslint-config-nicolaz/index.js
@@ -9,6 +9,9 @@ module.exports = {
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
 
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
+    'react/prop-types': 'off',
+
     // https://www.npmjs.com/package/eslint-plugin-react-hooks
     // https://reactjs.org/docs/hooks-rules.html
     'react-hooks/exhaustive-deps': 'warn'
