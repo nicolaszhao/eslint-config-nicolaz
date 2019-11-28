ed54d640a30c11d9b129a68eb3d186fb1407208b
Nicolas <zhaoxd@zenmen.com>
2019-11-28 14:39:31 +0800

    feat: label-has-associated-control -> off

--- packages/eslint-config-nicolaz/index.js
+++ packages/eslint-config-nicolaz/index.js
@@ -6,9 +6,15 @@ module.exports = {
   ],
   rules: {
 
+    // 目前使用下来有些问题:
+    // https://github.com/evcohen/eslint-plugin-jsx-a11y/issues/511
+    // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/HEAD/docs/rules/label-has-associated-control.md
+    'jsx-a11y/label-has-associated-control': 'off',
+
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
 
+    // 强制 props 不使用展开操作符，但允许自定义组件
     // https://github.com/yannickcr/eslint-plugin-react/blob/HEAD/docs/rules/jsx-props-no-spreading.md
     'react/jsx-props-no-spreading': ['error', {
       html: 'enforce',
